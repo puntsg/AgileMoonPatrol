a1eea623a935b535dde5a379c06fdedc43220f6a
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-03 18:49:47 +0100

    Functional enemies added

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,12 +1,24 @@
+export const LEVEL = {
+    SCROLL_SPEED: {
+        ENTITIES: 200,
+        BACKGROUND: 1.5,
+        FOREGROUND: 0.625
+    }
+}
+
+export const CHECKPOINT = {
+    SEPARATION: 2000
+}
+
 export const ENEMY = {
     UFO: {
-        SPEED: 1,
+        SPEED: 50,
         STATES: {
             ARRIVING: 0,
             PATROLLING: 1,
             STEERING: 2
         },
-        ARRIVING_TOLERANCE: 0.3,
-        PATROLLING_AMPLITUDE: 5
+        ARRIVING_TOLERANCE: 10,
+        PATROLLING_AMPLITUDE: 50
     }
 }
