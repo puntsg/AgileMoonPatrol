1c841afb2b0cd5eccb73edf3e385527e1731566d
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-03 11:00:40 +0100

    Started base for Enemy

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,3 +1,12 @@
-export const UFO = {
-    SPEED: 1
+export const ENEMY = {
+    UFO: {
+        SPEED: 1,
+        STATES: {
+            ARRIVING: 0,
+            PATROLLING: 1,
+            STEERING: 2
+        },
+        ARRIVING_TOLERANCE: 0.3,
+        PATROLLING_AMPLITUDE: 5
+    }
 }
