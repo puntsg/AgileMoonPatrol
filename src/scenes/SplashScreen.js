094c5416ba29bb2520159c911b8521e43df7d566
SergiGBerlanga-DESKTOP-10JPS7V\sergi <sergigonzalezberlanga@gmail.com>
2026-01-07 08:25:16 +0100

    Checkpoint progress restarted with new game

--- src/scenes/SplashScreen.js
+++ src/scenes/SplashScreen.js
@@ -93,6 +93,7 @@ export class SplashScreen extends Phaser.Scene {
         if(this.one.isDown){
             this.cameras.main.fadeOut(500, 0, 0, 0);
             this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.registry.set('Checkpoint', 0);
                 this.scene.start('MainGame');
             });
         }
